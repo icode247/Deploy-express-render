b07f6b0026c844ca2dfe9a144c52127e4d28ac26
Ekekenta Odionyenfe .C <60361247+icode247@users.noreply.github.com>
2022-10-28 00:02:44 +0100

    Update dataSource.ts

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm";
 export const myDataSource = new DataSource({
   type: "postgres",
   host: process.env.HOSTNAME,
-  port: 5432,
+  port: process.env.PORT,
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE_NAME,
